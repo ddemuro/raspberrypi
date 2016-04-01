2da18e7a3004886ac0bce62dd993a510bef26850
Derek Demuro <ddemuro@gmail.com>
2016-04-01 15:01:54 -0300

    Fix path's damn, can't get it right lol

--- compile-other/crelay-compile.sh
+++ compile-other/crelay-compile.sh
@@ -3,7 +3,7 @@
 #Control Relay module USB with Crelay     #
 ###########################################
 readonly libs='libftdi1 libftdi-dev libhidapi-libusb0 libhidapi-dev libusb-1.0-0 libusb-1.0-0-dev perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python libudev-dev libusb-1.0-0-dev libftdi1 libhidapi-libusb0 libusb-1.0-0 hidapi*'
-readonly install_path='/opt/raspberrypi/'
+readonly install_path='/opt/raspberrypi'
 readonly gitrepo='https://git@github.com:ddemuro/crelay.git'
 
 apt-get install $libs
